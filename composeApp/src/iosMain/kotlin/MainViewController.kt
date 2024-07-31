537eaeb2bd0968d5f29e8f9afc01054a0709c09d
sundar <sundar.s@applab.qa>
2024-07-31 18:44:24 +0530

    Battery level finder in android,ios,desktop applications

---
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -0,0 +1,4 @@
+import androidx.compose.runtime.remember
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App(batteryManager = remember { BatteryManager() }) }
